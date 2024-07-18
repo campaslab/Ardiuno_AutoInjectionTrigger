45a61cc14a4f2ed2f055c904d03a07df8c993d01
Petr Pospíšil <90824383+PospiP@users.noreply.github.com>
2024-07-18 17:34:29 +0200

    Update TTL_low.ino

--- TTL_low.ino
+++ TTL_low.ino
@@ -1,3 +1,12 @@
+/*
+Auto injection
+
+This code puts the desired output to LOW, meaning it stops sending TTL pulses or constant signal (HIGH).
+
+Created by Petr Pospisil on 18 July 2014
+
+*/
+
 int TTL = 10;
 
 void setup() {
